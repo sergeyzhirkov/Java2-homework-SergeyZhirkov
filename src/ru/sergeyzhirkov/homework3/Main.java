7a929faa0556d8f370a03822c4113bcc85b81bad
Sergey Zhirkov <zhirkov_55250@mail.ru>
2020-11-12 19:59:19 +0500

    add homework for lesson - 3

---
+++ src/ru/sergeyzhirkov/homework3/Main.java
@@ -0,0 +1,14 @@
+package ru.sergeyzhirkov.homework3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Set.of().getClass().getName());
+        List<List<String>> list;
+        Integer i = 0;
+        Integer k = i;
+        i++;
+        System.out.println(k + " " + i);
+    }
+}
